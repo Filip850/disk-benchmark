6477c8e2f3c2ad8c6b9650a6ae6476b5a7160a46
Filip850 <72305652+Filip850@users.noreply.github.com>
2025-07-01 10:15:54 +0200

    Update check-speed.sh

--- check-speed.sh
+++ check-speed.sh
@@ -1,49 +1,130 @@
 #!/bin/bash
 
+# Config
+SEQ_FILE="/tmp/fio_seq_testfile"
+RAND_FILE="/tmp/fio_rand_testfile"
+SEQ_SIZE="4G"
+RAND_RUNTIME="60s"
+TEST_COUNT=3
 LOGFILE="disk_benchmark_$(date +%F_%H-%M-%S).log"
-TESTFILE="/tmp/fio_testfile"
-SIZE="1G"
 
-# Check is fio installed
-if ! command -v fio &> /dev/null; then
-    echo "fio not found. Installing..."
-    if [ -f /etc/debian_version ]; then
-        sudo apt update && sudo apt install -y fio
-    elif [ -f /etc/redhat-release ]; then
-        sudo dnf install -y fio || sudo yum install -y fio
-    elif [ -f /etc/arch-release ]; then
-        sudo pacman -Sy --noconfirm fio
-    else
-        echo "Unsupported distro. Please install fio manually."
-        exit 1
-    fi
+# Parse args
+for arg in "$@"; do
+  case $arg in
+    --number-of-tests=*)
+      TEST_COUNT="${arg#*=}"
+      shift
+      ;;
+  esac
+done
+
+# Check fio
+if ! command -v fio &>/dev/null; then
+  echo "fio not found. Installing..."
+  if [ -f /etc/debian_version ]; then
+    sudo apt update && sudo apt install -y fio
+  elif [ -f /etc/redhat-release ]; then
+    sudo dnf install -y fio || sudo yum install -y fio
+  elif [ -f /etc/arch-release ]; then
+    sudo pacman -Sy --noconfirm fio
+  else
+    echo "Unsupported distro. Install fio manually."
+    exit 1
+  fi
 fi
 
-echo "Disk benchmark started: $(date)"
-echo "Log file: $LOGFILE"
-echo "----------------------------------"
+# Arrays to store results
+declare -a SEQ_READ_MB SEQ_WRITE_MB RAND_READ_MB RAND_WRITE_MB
+declare -a SEQ_READ_IOPS SEQ_WRITE_IOPS RAND_READ_IOPS RAND_WRITE_IOPS
+declare -a SEQ_READ_LAT SEQ_WRITE_LAT RAND_READ_LAT RAND_WRITE_LAT
 
+run_fio_seq_write() {
+  fio --name=seqw --size=$SEQ_SIZE --filename=$SEQ_FILE --bs=1M --rw=write --direct=1 --numjobs=1 --group_reporting --output-format=terse
+}
+
+run_fio_seq_read() {
+  fio --name=seqr --size=$SEQ_SIZE --filename=$SEQ_FILE --bs=1M --rw=read --direct=1 --numjobs=1 --group_reporting --output-format=terse
+}
+
+run_fio_rand_write() {
+  fio --name=randw --filename=$RAND_FILE --bs=4K --rw=randwrite --direct=1 --numjobs=1 --group_reporting --runtime=$RAND_RUNTIME --time_based --output-format=terse
+}
+
+run_fio_rand_read() {
+  fio --name=randr --filename=$RAND_FILE --bs=4K --rw=randread --direct=1 --numjobs=1 --group_reporting --runtime=$RAND_RUNTIME --time_based --output-format=terse
+}
+
+# Run tests
+echo "Running $TEST_COUNT iterations..."
+for ((i=1;i<=TEST_COUNT;i++)); do
+  echo "Iteration $i..."
+
+  OUT=$(run_fio_seq_write)
+  MBPS=$(( $(echo $OUT | cut -d';' -f49) / 1024 ))
+  IOPS=$(echo $OUT | cut -d';' -f48)
+  LAT=$(echo $OUT | cut -d';' -f52)
+  SEQ_WRITE_MB+=($MBPS)
+  SEQ_WRITE_IOPS+=($IOPS)
+  SEQ_WRITE_LAT+=($LAT)
+
+  OUT=$(run_fio_seq_read)
+  MBPS=$(( $(echo $OUT | cut -d';' -f7) / 1024 ))
+  IOPS=$(echo $OUT | cut -d';' -f6)
+  LAT=$(echo $OUT | cut -d';' -f10)
+  SEQ_READ_MB+=($MBPS)
+  SEQ_READ_IOPS+=($IOPS)
+  SEQ_READ_LAT+=($LAT)
+
+  OUT=$(run_fio_rand_write)
+  MBPS=$(( $(echo $OUT | cut -d';' -f49) / 1024 ))
+  IOPS=$(echo $OUT | cut -d';' -f48)
+  LAT=$(echo $OUT | cut -d';' -f52)
+  RAND_WRITE_MB+=($MBPS)
+  RAND_WRITE_IOPS+=($IOPS)
+  RAND_WRITE_LAT+=($LAT)
+
+  OUT=$(run_fio_rand_read)
+  MBPS=$(( $(echo $OUT | cut -d';' -f7) / 1024 ))
+  IOPS=$(echo $OUT | cut -d';' -f6)
+  LAT=$(echo $OUT | cut -d';' -f10)
+  RAND_READ_MB+=($MBPS)
+  RAND_READ_IOPS+=($IOPS)
+  RAND_READ_LAT+=($LAT)
+done
+
+# Function for mean + stdev
+calc_stats() {
+  local arr=("$@")
+  local sum=0
+  local count=${#arr[@]}
+  for v in "${arr[@]}"; do sum=$((sum + v)); done
+  local mean=$((sum / count))
+
+  local sumsq=0
+  for v in "${arr[@]}"; do
+    diff=$((v - mean))
+    sumsq=$((sumsq + diff * diff))
+  done
+  local stdev=0
+  if (( count > 1 )); then
+    stdev=$(echo "scale=2; sqrt($sumsq / ($count - 1))" | bc)
+  fi
+  echo "$mean ± $stdev"
+}
+
+# Output results
 {
 echo "Disk Benchmark Report - $(date)"
-echo "Test file: $TESTFILE ($SIZE)"
 echo ""
-echo -e "Test\t\t\tRead MB/s\tWrite MB/s"
-
-# Sequential 1M
-SEQ=$(fio --name=seq --size=$SIZE --filename=$TESTFILE --bs=1M --rw=rw --direct=1 --numjobs=1 --group_reporting --output-format=terse)
-SEQ_READ=$(echo $SEQ | cut -d';' -f7)
-SEQ_WRITE=$(echo $SEQ | cut -d';' -f49)
-echo -e "Sequential 1M\t$((SEQ_READ/1024))\t\t$((SEQ_WRITE/1024))"
-echo -e "Sequential 1M\t$((SEQ_READ/1024))\t\t$((SEQ_WRITE/1024))" >> $LOGFILE
-
-# Random 4K
-RAND=$(fio --name=rand --size=$SIZE --filename=$TESTFILE --bs=4K --rw=randrw --direct=1 --numjobs=1 --group_reporting --output-format=terse)
-RAND_READ=$(echo $RAND | cut -d';' -f7)
-RAND_WRITE=$(echo $RAND | cut -d';' -f49)
-echo -e "Random 4K\t\t$((RAND_READ/1024))\t\t$((RAND_WRITE/1024))"
-echo -e "Random 4K\t\t$((RAND_READ/1024))\t\t$((RAND_WRITE/1024))" >> $LOGFILE
-
-rm -f $TESTFILE
+printf "%-20s %-15s %-15s %-15s %-15s\n" "Test" "MB/s" "IOPS" "Avg Lat (ms)" ""
+echo "------------------------------------------------------------------------------------------"
+printf "%-20s %-15s %-15s %-15s\n" "Sequential 1M Read" "$(calc_stats "${SEQ_READ_MB[@]}")" "$(calc_stats "${SEQ_READ_IOPS[@]}")" "$(calc_stats "${SEQ_READ_LAT[@]}")"
+printf "%-20s %-15s %-15s %-15s\n" "Sequential 1M Write" "$(calc_stats "${SEQ_WRITE_MB[@]}")" "$(calc_stats "${SEQ_WRITE_IOPS[@]}")" "$(calc_stats "${SEQ_WRITE_LAT[@]}")"
+printf "%-20s %-15s %-15s %-15s\n" "Random 4K Read" "$(calc_stats "${RAND_READ_MB[@]}")" "$(calc_stats "${RAND_READ_IOPS[@]}")" "$(calc_stats "${RAND_READ_LAT[@]}")"
+printf "%-20s %-15s %-15s %-15s\n" "Random 4K Write" "$(calc_stats "${RAND_WRITE_MB[@]}")" "$(calc_stats "${RAND_WRITE_IOPS[@]}")" "$(calc_stats "${RAND_WRITE_LAT[@]}")"
 echo ""
 echo "Results saved to $LOGFILE"
 } | tee -a $LOGFILE
+
+# Cleanup
+rm -f $SEQ_FILE $RAND_FILE
